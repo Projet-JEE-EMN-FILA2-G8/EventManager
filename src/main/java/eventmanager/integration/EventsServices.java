df72a4385b102a4a638aac1b4df0adbed0828c7f
Adrian <slimwhity@gmail.com>
2014-10-30 11:04:01 +0100

    Ajout javadoc à l'interface EventsServices

--- src/main/java/eventmanager/integration/EventsServices.java
+++ src/main/java/eventmanager/integration/EventsServices.java
@@ -10,29 +10,33 @@ import eventmanager.integration.bean.UserBean;
 public interface EventsServices {
 	
 	/**
-	 * @param event
-	 * @return
+	 * Créer un evenement.
+	 * @param event l'evenement à créér
+	 * @return true si la création a réussi
 	 */
 	public EventBean createEvent(EventBean event);
 	/**
-	 * @param event
+	 * Publie un evenement. Marque l'indicateur visible à True et génère une URL
+	 * @param event l'evenement à publier
 	 * @return
 	 */
 	public URL publishEvent(EventBean event);
 	/**
-	 * @param user
-	 * @return
+	 * Retourne une liste d'evenement dont l'hôte est l'utilisateur passé en paramètre
+	 * @param user l'utilisateur hôte des evenements à rechercher
+	 * @return la liste des evenements créés par l'utilisateur
 	 */
 	public List<EventBean> getHostedEvents(UserBean user);
 	/**
-	 * @param url
-	 * @return
+	 * @param url l'URL de l'evenement
+	 * @return le bean de l'evenement pointé par l'url, null s'il n'existe pas
 	 */
 	public EventBean getEventByUrl(URL url);
 	/**
-	 * @param event
-	 * @param participant
-	 * @return
+	 * Enregistre un participant à un evenement
+	 * @param event l'evenement auquel le participant s'inscrit
+	 * @param participant le participant à inscrire
+	 * @return true si l'inscription a réussi
 	 */
 	public boolean registerParticipant(EventBean event, ParticipantBean participant);
 
